423023a36762286b481a3d1b334f39274a00e7b2
Junichi TANAKA <junsan45@gmail.com>
2022-09-10 18:52:04 +0900

    chore(fish): Add fish

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 
 .PHONY: install
+install: vim
 
-install: ~/.vim
-
-~/.vim:
-	ln -s $(CURDIR)/vim ~/.vim
+.PHONY: vim
+vim:
+	ln -sh $(CURDIR)/vim ~/.vim
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+.PHONY: fish
+fish:
+	mkdir -p ~/.config
+	ln -sh $(CURDIR)/fish ~/.config/fish
+	fish -c "curl -sL git.io/fisher | source && fisher update"
---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+
+set -x SHELL /opt/homebrew/bin/fish
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    eval (/opt/homebrew/bin/brew shellenv)
+
+    set -g theme_display_git_default_branch yes
+    set -g theme_color_scheme dark
+
+    set -Ux fish_user_paths $HOME/.anyenv/bin $fish_user_paths
+    source (anyenv init -|psub)
+    source (direnv hook fish |psub)
+
+    set -g GHQ_SELECTOR fzf
+    bind \cg __ghq_repository_search
+
+    source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+end
